601b86f6a4f57dfb4570988e4bb40b6790c9168e
Nick Lanese <nlanese21@gmail.com>
2020-02-23 03:22:49 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,7 +9,8 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if (turn_count(board) % 2) == 0
+  player = turn_count(board)
+  if (player % 2) == 0
     return "X"
   else
     return "O"
